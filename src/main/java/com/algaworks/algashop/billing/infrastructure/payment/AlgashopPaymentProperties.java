c64eda48baa5685ec856463846cfbaeefd87dc51
Breno Pedro <brenopedro14@hotmail.com>
2026-06-01 22:37:20 -0300

    feat: criado integração inicial com fastpay

---
+++ src/main/java/com/algaworks/algashop/billing/infrastructure/payment/AlgashopPaymentProperties.java
@@ -0,0 +1,36 @@
+package com.algaworks.algashop.billing.infrastructure.payment;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Data
+@Component
+@Validated
+@ConfigurationProperties("algashop.integrations.payment")
+public class AlgashopPaymentProperties {
+
+    @NotNull
+    private AlgashopPaymentProvider provider;
+
+    @NotNull
+    private FastpayProperties fastpay;
+
+    public enum AlgashopPaymentProvider {
+        FAKE,
+        FASTPAY
+    }
+
+    @Validated
+    @Data
+    public static class FastpayProperties {
+        @NotBlank
+        private String hostname;
+
+        @NotBlank
+        private String privateToken;
+    }
+}

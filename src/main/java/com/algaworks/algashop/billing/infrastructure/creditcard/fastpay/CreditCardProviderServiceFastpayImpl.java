f440de317e334a481e90659330f9bd046cfa9544
Breno Pinto <brenopedro14@hotmail.com>
2026-06-02 10:34:16 -0300

    feat: integração com fastapi e testes de integração

--- src/main/java/com/algaworks/algashop/billing/infrastructure/creditcard/fastpay/CreditCardProviderServiceFastpayImpl.java
+++ src/main/java/com/algaworks/algashop/billing/infrastructure/creditcard/fastpay/CreditCardProviderServiceFastpayImpl.java
@@ -2,27 +2,55 @@ package com.algaworks.algashop.billing.infrastructure.creditcard.fastpay;
 
 import com.algaworks.algashop.billing.domain.model.creditcard.CreditCardProviderService;
 import com.algaworks.algashop.billing.domain.model.creditcard.LimitedCreditCard;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.Optional;
 import java.util.UUID;
 
 @Service
 @ConditionalOnProperty(name = "algashop.integrations.payment.provider", havingValue = "FASTPAY")
+@RequiredArgsConstructor
 public class CreditCardProviderServiceFastpayImpl implements CreditCardProviderService {
+
+    private final FastpayCreditCardAPIClient fastpayAPIClient;
+
     @Override
     public LimitedCreditCard register(UUID customerId, String tokenizedCard) {
-        return null;
+        FastpayCreditCardInput input = FastpayCreditCardInput.builder()
+                .tokenizedCard(tokenizedCard)
+                .customerCode(customerId.toString())
+                .build();
+
+        FastpayCreditCardResponse response = fastpayAPIClient.create(input);
+        return toLimitedCreditCard(response);
     }
 
     @Override
-    public Optional<LimitedCreditCard> findById(String providerCreditCar) {
-        return Optional.empty();
+    public Optional<LimitedCreditCard> findById(String gatewayCode) {
+        FastpayCreditCardResponse response;
+        try {
+            response = fastpayAPIClient.findById(gatewayCode);
+        } catch (HttpClientErrorException.NotFound e) {
+            return Optional.empty();
+        }
+        return Optional.of(toLimitedCreditCard(response));
     }
 
     @Override
     public void delete(String gatewayCode) {
+        fastpayAPIClient.delete(gatewayCode);
+    }
 
+    private static LimitedCreditCard toLimitedCreditCard(FastpayCreditCardResponse response) {
+        return LimitedCreditCard.builder()
+                .lastNumbers(response.getLastNumbers())
+                .expMonth(response.getExpMonth())
+                .expYear(response.getExpYear())
+                .brand(response.getBrand())
+                .gatewayCode(response.getId())
+                .build();
     }
 }
---
+++ src/test/java/com/algaworks/algashop/billing/infrastructure/creditcard/fastpay/CreditCardProviderServiceFastpayImplIT.java
@@ -0,0 +1,70 @@
+package com.algaworks.algashop.billing.infrastructure.creditcard.fastpay;
+
+import com.algaworks.algashop.billing.domain.model.creditcard.LimitedCreditCard;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+import java.time.Year;
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Import(FastpayCreditCardTokenizationAPIClientConfig.class)
+class CreditCardProviderServiceFastpayImplIT {
+
+    @Autowired
+    private CreditCardProviderServiceFastpayImpl creditCardProvider;
+
+    @Autowired
+    private FastpayCreditCardTokenizationAPIClient tokenizationAPIClient;
+
+    private static final UUID validCustomerId = UUID.randomUUID();
+    private static final String alwaysPaidCardNumber = "4622943127011022";
+
+    @Test
+    void shouldCreateCreditCard() {
+        LimitedCreditCard limitedCreditCard = registerCard();
+
+        assertThat(limitedCreditCard.getGatewayCode()).isNotBlank();
+    }
+
+    @Test
+    void shouldFindRegisteredCreditCard() {
+        LimitedCreditCard limitedCreditCard = registerCard();
+
+        LimitedCreditCard limitedCreditCardFound = creditCardProvider.findById(limitedCreditCard.getGatewayCode()).orElseThrow();
+
+        assertThat(limitedCreditCardFound.getGatewayCode()).isEqualTo(limitedCreditCard.getGatewayCode());
+    }
+
+
+    @Test
+    void shouldDeleteRegisteredCreditCard() {
+        LimitedCreditCard limitedCreditCard = registerCard();
+
+        creditCardProvider.delete(limitedCreditCard.getGatewayCode());
+
+        Optional<LimitedCreditCard> possibleCreditCard = creditCardProvider.findById(limitedCreditCard.getGatewayCode());
+
+        assertThat(possibleCreditCard).isEmpty();
+    }
+
+    private LimitedCreditCard registerCard() {
+        FastpayTokenizationInput input = FastpayTokenizationInput.builder()
+                .number(alwaysPaidCardNumber)
+                .cvv("222")
+                .expMonth(1)
+                .holderName("John Doe")
+                .holderDocument("12345")
+                .expYear(Year.now().getValue() + 5)
+                .build();
+
+        FastpayTokenizedCreditCardModel response = tokenizationAPIClient.tokenize(input);
+        return creditCardProvider.register(validCustomerId, response.getTokenizedCard());
+    }
+
+}

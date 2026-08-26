bbc41f4eca2c5260e032593d7ca4629e00d18c2a
Breno Pedro Assis Tenório Pinto <brenopedro14@hotmail.com>
2026-08-26 20:13:17 -0300

    feat: ajustando teste de billing

--- src/test/java/com/algaworks/algashop/billing/application/invoice/AbstractApplicationIT.java
+++ src/test/java/com/algaworks/algashop/billing/application/invoice/AbstractApplicationIT.java
@@ -4,6 +4,8 @@ import com.algaworks.algashop.billing.utils.TestContainerPostgreSQLConfig;
 import org.springframework.boot.jdbc.test.autoconfigure.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.transaction.annotation.Transactional;
 
 @Transactional
@@ -11,4 +13,7 @@ import org.springframework.transaction.annotation.Transactional;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class AbstractApplicationIT {
+
+    @MockitoBean
+    private JwtDecoder jwtDecoder;
 }
